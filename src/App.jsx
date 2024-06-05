c06f8fbab210c24dd04d606d12a2b4e720312025
helder mabasso <hfmabasso@gmail.com>
2024-06-05 22:23:36 +0200

    Context Basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter,Routes,Route,Link } from 'react-router-dom'
+import { createContext } from 'react'
 import './App.css'
 import Home from './home'
 import Products from './products'
 import About from './about'
+
+const UserContext = createContext(null)
 
 function App() {
   
@@ -11,15 +14,20 @@ function App() {
       <div>
 
         <h1>Routing - Hello World</h1>
+
         <Link to='/'>Home</Link>--
         <Link to='/about'>About</Link>--
         <Link to='/products'>Products</Link>--
 
-        <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/about/' element={<About/>}></Route>
-          <Route path='/products/' element={<Products/>}></Route>
-        </Routes>
+        <hr/>
+
+        <UserContext.Provider value={{users:['Helder']}}>
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/about/' element={<About/>}></Route>
+            <Route path='/products/' element={<Products/>}></Route>
+          </Routes>
+        </UserContext.Provider>
 
       </div>
     </BrowserRouter>

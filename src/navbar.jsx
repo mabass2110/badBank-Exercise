c216e0f09bed01fc3b3117a2df7474070f1d158c
mabass2110 <131558331+mabass2110@users.noreply.github.com>
2024-06-08 23:35:13 +0200

    Update navbar.jsx

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,19 +1,23 @@
 import { Link } from "react-router-dom";
+
+//Navigation component
 export default function Navigation(){
 
     return(
 
         <ul className="nav">
+
+            
             <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/">Home</Link>
             </li>
 
             <li className="nav-item">
-            <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/about">About</Link>
             </li>
 
             <li className="nav-item">
-             <Link className="nav-link" to="/products">Products</Link>
+                 <Link className="nav-link" to="/products">Products</Link>
             </li>
 
         </ul>
